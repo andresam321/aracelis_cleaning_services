2a867ffc88cacc02fe8c50255f09250261f60a6a
Andres Mercado <andresmercado96@gmail.com>
2024-10-16 11:19:10 -0700

    config

--- app/config.py
+++ app/config.py
@@ -5,6 +5,9 @@ class Config:
     FLASK_RUN_PORT = os.environ.get('FLASK_RUN_PORT', '5000')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = True
+    SESSION_COOKIE_SECURE = True
+    REMEMBER_COOKIE_SECURE = True
+    PREFERRED_URL_SCHEME = 'https'
 
     # Check if DATABASE_URL is defined before setting SQLALCHEMY_DATABASE_URI
     database_url = os.environ.get('DATABASE_URL')
